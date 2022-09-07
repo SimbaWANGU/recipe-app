403a9685fc0c8c952a3cec5868fab0e74ec893d1
SimbaWANGU <simbawangulu@gmail.com>
2022-09-07 10:53:46 +0300

    add food controller method, route and view

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,2 +1,11 @@
 class FoodController < ApplicationController
+  def index
+    @all_food = Food.all
+  end
+
+  def delete
+    @food_to_delete = Food.find(params[:id])
+    @food_to_delete.destroy
+    redirect_to food_path
+  end
 end
