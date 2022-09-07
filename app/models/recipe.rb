9ba01d7b50f04b88b31c5255f7b52bd52e177fa6
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-09-07 21:34:26 +0100

    Add validation for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
+  validates :name, :preparation_time, :cooking_time, :description, presence: true
   belongs_to :user, foreign_key: 'user_id'
   has_many :recipe_foods, foreign_key: 'recipe_id'
 end
