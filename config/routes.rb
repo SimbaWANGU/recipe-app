bf230991ef86cc3ea9308df7524feb7209b35164
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-09-07 15:20:48 +0100

    Updates routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   delete 'recipes/:id', to: 'recipes#destroy'
   patch 'recipes/:id', to: 'recipeses#update'
-  get 'recipes/:id/edit', to: 'recipes#edit'
+  get 'recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
   post '/recipes', to: 'recipes#create'
   get '/recipes', to: 'recipes#index'
   get '/recipes/new', to: 'recipes#new'
