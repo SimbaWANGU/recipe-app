568f9d2cf64fa2b42bf6a9259b543819409ab0c5
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-09-07 15:20:01 +0100

    Add recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,5 +3,28 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
-  
+  def show
+    @recipe = Recipe.find(params[:id])
+    @quantity = RecipeFood.find_by(recipe_id: params[:id]).quantity
+    @price = Food.find_by(id: RecipeFood.find_by(recipe_id: params[:id]).food_id).price
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(articles_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :content)
+  end
 end
