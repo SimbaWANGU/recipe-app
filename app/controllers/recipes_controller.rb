3e1803ced36f5418c4a58a8616e9aac64f217846
SimbaWANGU <simbawangulu@gmail.com>
2022-09-08 12:32:28 +0300

    add public recipe controller method in recipecontroller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
   def public_recipes
-    
+    @public_recipe = Recipe.where(public: "true").includes(:recipe_foods).order(updated_at: :desc)
   end
 end
