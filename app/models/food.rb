96c46d2c2188b3b4a34084c691c0e2583f30205e
SimbaWANGU <simbawangulu@gmail.com>
2022-09-08 12:30:15 +0300

    modify models to ensure table associations works as expected

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
-  has_many :recipe_foods, foreign_key: 'food_id', dependent: :delete_all
+  has_many :recipes, through: :recipe_foods, dependent: :delete_all
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
-  has_many :recipe_foods, foreign_key: 'recipe_id'
+  has_many :food, through: :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
 end
