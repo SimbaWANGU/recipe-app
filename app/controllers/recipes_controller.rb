a2e4f8ada4a9d87f6ba14b81ce06ac23a915d054
SimbaWANGU <simbawangulu@gmail.com>
2022-09-09 16:26:27 +0300

    add shopping_list controller method in recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -43,9 +43,54 @@ class RecipesController < ApplicationController
     @public_recipe = Recipe.where(public: 'true').includes(:recipe_foods).order(updated_at: :desc)
   end
 
+  def shopping_list
+    @recipe_shopping = Recipe.find(params[:id]).recipe_foods
+    @food = []
+    @recipe_shopping.ids.each do |id|
+      @food.push(Food.find_by(id: id))
+    end
+    @user_food = current_user.food
+    @comparison_food = custom_difference(@food, @user_food)
+    same_food_result = same_food(@food, @user_food)
+    if same_food_result == 0 
+      return @comparison_food
+    else
+      @comparison_food.append(same_food(@food, @user_food))
+    end
+  end
+
   private
 
   def recipes_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
+
+  def custom_difference(all, subset)
+    diff = all.reject { |all_curr|
+      subset.find{ |subset_curr|
+        subset_curr.name == all_curr.name
+      } != nil
+    }
+  end
+
+  def same_food(food, user_food)
+    @comparis_food = []
+    @user_food.each do |uf|
+      @food.each do |rf|
+        if uf.name == rf.name
+          if (uf.quantity - rf.quantity) == 0
+            next
+          else
+            uf.quantity = (uf.quantity - rf.quantity).abs
+            @comparis_food.push(uf)
+          end
+        end
+      end
+    end
+    unless @comparis_food.count == 0 
+      return @comparis_food
+    else
+      return 0
+    end
   end
 end
